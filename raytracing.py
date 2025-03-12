c895c677e1f9e5101971619b4cf01e1724edd932
Dex42xp <dex42xp@gmail.com>
2025-03-12 08:12:53 +0300

    Add files via upload

--- raytracing.py
+++ raytracing.py
@@ -2,38 +2,50 @@ import pygame as pg
 import numba as nb
 import numpy as np
 
-#class Object():
 
-class Rasterization():
-    def __init__(self, app):
-        self.app = app
+class Scene:
+    def __init__(self):
+        self.objects = []
+        self.lights = []
 
-class RayTracing():
-    def __init__(self, app):
-        self.app = app
+    def add_object(self, obj):
+        self.objects.append(obj)
 
-class App():
-    def __init__(self, res=(1600,900)):
+    def add_light(self, light):
+        self.lights.append(light)
+        
+class PointLight:
+    def __init__(self, position, color):
+        self.position = position
+        self.color = color
+        
+class Camera:
+    def __init__(self,position):
+        self.position = position
+
+
+class Application():
+    def __init__(self):
         pg.init()
-        self.RES = self.WIDTH, self.HEIGHT = res
-        self.H_WIDTH, self.H_HEIGHT = self.WIDTH//2, self.HEIGHT//2
+        self.RES = self.WIDTH, self.HEIGHT = 1600, 900
         self.FPS = 500
         self.screen = pg.display.set_mode(self.RES)
         self.clock = pg.time.Clock()
         self.running = True
         
+    def draw(self):
+        self.screen.fill((0,0,0))
+        #pg.surfarray.blit_array(self.screen, )
+        
     def run(self):
         self.running = True
         while self.running:
             [exit() for i in pg.event.get() if i.type == pg.QUIT]
-            self.screen.fill((0,0,0))
-            pg.display.set_caption("FPS: " + str(self.clock.get_fps()))
+            self.draw()
+            pg.display.set_caption(str(self.clock.get_fps()))
             pg.display.flip()
             self.clock.tick(self.FPS)
             
-            
 if __name__ == "__main__":
-    app = App()
-    rasterization = Rasterization(app)
-    rayTracing = RayTracing(app)
+    app = Application()
     app.run()
